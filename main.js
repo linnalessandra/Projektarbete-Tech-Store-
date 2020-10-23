32df31652d6cae43f4830222767659318133ec3f
Sara Oscarsson <oscarsson.sara@hotmail.com>
2020-10-23 13:57:57 +0200

    functions for counting products
    created a function to put a "clicked on" product o cart and clickercounter

--- main.js
+++ main.js
@@ -4,24 +4,20 @@ var listOfProducts;
 function loadProducts() {
     fetch("./products.json")
     .then(function(response) {
-        return response.json();
-        
+        return response.json();       
     })
     .then(function(products) {
         listOfProducts = products;
         for(var i = 0; i <listOfProducts.length;i++) {addProductsToWebpage(i)}
     });
 }
-
-
+/* starts when siteloads */
 function initSite() {
     loadProducts();
-    // This would also be a good place to initialize other parts of the UI
+    clickCounter()
 }
-
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage(i) {
-/*     let main=document.getElementById("main") */
     let body=document.body
     /* Main container */
     let mainContainer=document.createElement("div")
@@ -57,12 +53,35 @@ function addProductsToWebpage(i) {
     priceDiv.appendChild(price)
     priceDiv.appendChild(button)
     photoDiv.appendChild(photo)
-
-
-    // Check your console to see that the products are stored in the listOfProducts varible.
-
+    //function that puts a clicked on product in cart
+    button.addEventListener("click", function(){
+        let cart = {
+            title: listOfProducts[i].title,
+            description: listOfProducts[i].description,
+            image: listOfProducts[i].image,
+            price: listOfProducts[i].price
+        }
+        let currentShoppingCart = JSON.parse(localStorage.getItem("shoppingcart"))
+        if(currentShoppingCart == null){
+            currentShoppingCart = []
+        }
+        currentShoppingCart.push(cart)
+        localStorage.setItem("shoppingcart", JSON.stringify(currentShoppingCart))
+        location.reload()
+    })   
+}
+    //function that counts products in cart
+    function clickCounter(){
+        let countProducts = JSON.parse(localStorage.getItem("shoppingcart"))
+        let productsInCart = document.getElementById("productsInCart")
+        let clicks = 0
+        if(countProducts != null){
+            clicks = countProducts.length
+            productsInCart.innerHTML = clicks
+        } else if(countProducts == 0){
+            productsInCart.innerHTML = clicks
+        }
     }
- 
 
     // Add your code here, remember to brake your code in to smaller function blocks
     // to reduce complexity and increase readability. Each function should have
